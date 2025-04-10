db4e764276640f28b6cc577c7ffc3bf515e72921
Serhii <messileonl@gmail.com>
2025-04-10 18:06:57 +0200

    update max images

--- routes/api/images.routes.js
+++ routes/api/images.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { deleteImages, uploadImages, updateImages, getImages } from "../../controllers/images.js";
 import multer from "multer";
 const route = new Router();
+const MAX_IMAGES = 20;
+
 
 route.delete("/", deleteImages);
 route.get("/getImages", getImages);
-route.post("/", multer().array("images", 10), uploadImages);
-route.put("/",multer().array("images", 10), updateImages);
+route.post("/", multer().array("images", MAX_IMAGES), uploadImages);
+route.put("/",multer().array("images", MAX_IMAGES), updateImages);
 
 export default route;
